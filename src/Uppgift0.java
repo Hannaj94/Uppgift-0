0bfb957d4275b8ee2ec6947a8dd71c811767679d
Hannaj94 <56591980+Hannaj94@users.noreply.github.com>
2019-10-15 15:07:27 +0200

    Added some cod
    Added some very good code!

---
+++ src/Uppgift0.java
@@ -0,0 +1,10 @@
+public class Uppgift0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("How much is 2+2?");
+		System.out.println(2+2);
+	}
+
+}
